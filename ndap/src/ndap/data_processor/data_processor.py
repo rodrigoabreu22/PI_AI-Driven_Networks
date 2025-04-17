5345c87fdd7c042866886770ef4132c72ed7c75f
xHuGODx <hugo.ribeiro04@ua.pt>
2025-04-17 18:32:50 +0100

    nProbe menos aqieles 2

--- ndap/src/ndap/data_processor/data_processor.py
+++ ndap/src/ndap/data_processor/data_processor.py
@@ -8,7 +8,7 @@ def run_nprobe(input_pcap):
     command = [
         "nprobe", "-i", input_pcap, "-P", "./features", "--csv-separator", ",", "--dont-reforge-timestamps",
         "-V", "9", "-T", 
-        "%FLOW_START_MILLISECONDS %FLOW_END_MILLISECONDS %IPV4_SRC_ADDR %L4_SRC_PORT %IPV4_DST_ADDR %L4_DST_PORT %PROTOCOL %L7_PROTO %IN_BYTES %IN_PKTS %OUT_BYTES %OUT_PKTS %TCP_FLAGS %CLIENT_TCP_FLAGS %SERVER_TCP_FLAGS %FLOW_DURATION_MILLISECONDS %DURATION_IN %DURATION_OUT %MIN_TTL %MAX_TTL %LONGEST_FLOW_PKT %SHORTEST_FLOW_PKT %MIN_IP_PKT_LEN %MAX_IP_PKT_LEN %SRC_TO_DST_SECOND_BYTES %DST_TO_SRC_SECOND_BYTES %RETRANSMITTED_IN_BYTES %RETRANSMITTED_IN_PKTS %RETRANSMITTED_OUT_BYTES %RETRANSMITTED_OUT_PKTS %SRC_TO_DST_AVG_THROUGHPUT %DST_TO_SRC_AVG_THROUGHPUT %NUM_PKTS_UP_TO_128_BYTES %NUM_PKTS_128_TO_256_BYTES %NUM_PKTS_256_TO_512_BYTES %NUM_PKTS_512_TO_1024_BYTES %NUM_PKTS_1024_TO_1514_BYTES %TCP_WIN_MAX_IN %TCP_WIN_MAX_OUT %ICMP_TYPE %ICMP_IPV4_TYPE %DNS_QUERY_ID %DNS_QUERY_TYPE %DNS_TTL_ANSWER %FTP_COMMAND_RET_CODE"
+        "%FLOW_START_MILLISECONDS %FLOW_END_MILLISECONDS %IPV4_SRC_ADDR %L4_SRC_PORT %IPV4_DST_ADDR %L4_DST_PORT %PROTOCOL %L7_PROTO %IN_BYTES %IN_PKTS %OUT_BYTES %OUT_PKTS %TCP_FLAGS %CLIENT_TCP_FLAGS %SERVER_TCP_FLAGS %FLOW_DURATION_MILLISECONDS %DURATION_IN %DURATION_OUT %MIN_TTL %MAX_TTL %LONGEST_FLOW_PKT %SHORTEST_FLOW_PKT %MIN_IP_PKT_LEN %MAX_IP_PKT_LEN %RETRANSMITTED_IN_BYTES %RETRANSMITTED_IN_PKTS %RETRANSMITTED_OUT_BYTES %RETRANSMITTED_OUT_PKTS %SRC_TO_DST_AVG_THROUGHPUT %DST_TO_SRC_AVG_THROUGHPUT %NUM_PKTS_UP_TO_128_BYTES %NUM_PKTS_128_TO_256_BYTES %NUM_PKTS_256_TO_512_BYTES %NUM_PKTS_512_TO_1024_BYTES %NUM_PKTS_1024_TO_1514_BYTES %TCP_WIN_MAX_IN %TCP_WIN_MAX_OUT %ICMP_TYPE %ICMP_IPV4_TYPE %DNS_QUERY_ID %DNS_QUERY_TYPE %DNS_TTL_ANSWER %FTP_COMMAND_RET_CODE"
     ]
     
     subprocess.run(command, stderr=subprocess.PIPE, text=True)
@@ -54,14 +54,6 @@ def move_csv_files():
     shutil.rmtree("./features")
     os.makedirs("./features")
 
-    
-def analyze_output(csv_file):
-    try:
-        df = pd.read_csv(csv_file, delimiter=",")
-        print("Data Shape:", df.shape)
-
-    except Exception as e:
-        print("Error loading CSV:", e)
 
 def addGT(csv_file):
     protocol_map = {
@@ -116,12 +108,10 @@ def addGT(csv_file):
 
 
 if __name__ == "__main__":
-    run_nprobe("1.pcap")
+    run_nprobe("a.pcap")
     print("Waiting for nprobe to finish...")
     move_csv_files()
     print("CSV files moved and merged.")
-    analyze_output("temp.flows")
-    print("Analyzing output...")
     addGT("temp.flows")
     print("Ground truth added to extracted.flows.")
 
