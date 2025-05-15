893c952fad9b3315b92eaa5ddbc3d5cebd4e419d
odraude23 <eduardolplopes@ua.pt>
2025-05-15 11:38:25 +0000

    API do receiver a funcionar e a meter os dados no kafka assinconamente. Falta inserir a lógica de comunicar com o NRF e meter tudo no docker compose

--- ndap/src/ndap/data_receiver/data_receiver_api.py
+++ ndap/src/ndap/data_receiver/data_receiver_api.py
@@ -1,11 +1,21 @@
-import requests
+import asyncio
+import base64
+import logging
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-import logging
+from kafka import KafkaProducer
 from contextlib import asynccontextmanager
 import uvicorn
+import requests
+from kafka.admin import KafkaAdminClient, NewTopic
 
 SUBSCRIPTION_URL = "http://localhost:8071/nnwdaf-eventssubscription/v1/subscriptions"
+
+TOPIC_PUSH = "RAW_NETWORK_DATA_RECEIVED"
+BROKER = "localhost:29092"
+
+packet_queue = asyncio.Queue()
+producer = None
 
 logging.basicConfig(
     filename='logs/data_receiver.log',
@@ -13,8 +23,57 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
+def create_topic(topic_name, broker, num_partitions=1, replication_factor=1):
+    try:
+        admin_client = KafkaAdminClient(bootstrap_servers=broker)
+        existing_topics = admin_client.list_topics()
+        if topic_name in existing_topics:
+            logging.info(f"Topic '{topic_name}' already exists.")
+        else:
+            topic = NewTopic(name=topic_name, num_partitions=num_partitions, replication_factor=replication_factor)
+            admin_client.create_topics([topic])
+            logging.info(f"Topic '{topic_name}' created successfully.")
+    except Exception as e:
+        logging.error(f"Failed to create topic '{topic_name}': {e}")
+    finally:
+        try:
+            admin_client.close()
+        except Exception as e:
+            logging.error(f"Can not close admin client. Error: {e}")
+            pass
+
+def create_kafka_producer():
+    producer = KafkaProducer(
+        bootstrap_servers=BROKER,
+        value_serializer=lambda v: v  # Raw bytes
+    )
+    logging.info("Kafka producer started.")
+    return producer
+
+async def kafka_worker():
+    global producer
+
+    while True:
+        packet_data = await packet_queue.get()
+        logging.info(f"Processing packet: {packet_data}")
+        try:
+            value = packet_data["value"]
+            headers = packet_data["headers"]
+            logging.info(f"headers: {headers}")
+            producer.send(TOPIC_PUSH, value=value, headers=headers)
+
+        except Exception as e:
+            logging.error(f"Kafka send failed: {e}")
+
+        packet_queue.task_done()
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    global producer
+    create_topic(TOPIC_PUSH, BROKER)
+    producer = create_kafka_producer()
+    asyncio.create_task(kafka_worker())
+
     payload = {
         "event": "PDU_SESSION_TRAFFIC",
         "notificationURI": "http://localhost:8072/nwdaf-collector-notify"
@@ -29,20 +88,30 @@ async def lifespan(app: FastAPI):
     
     yield
 
+    producer.flush()
+    producer.close()
+    logging.info("Kafka producer shut down.")
     logging.info("Shutting down app.")
 
 
 app = FastAPI(lifespan=lifespan)
 
 @app.post("/nwdaf-collector-notify")
-def receive_data(data: dict):
+async def receive_data(data: dict):
     """
     Endpoint to receive data from the NWDAF collector.
     """
     try:
-        # Process the incoming data
-        logging.info(f"Received data: {data}")
-        # Here you can add your logic to handle the received data
+        packets = data.get("pcap", [])
+        for pkt in packets:
+            raw_bytes = base64.b64decode(pkt["pcap_bytes"])
+            timestamp = pkt.get("timestamp", "")
+
+            await packet_queue.put({
+                "value": raw_bytes,
+                "headers": [("timestamp", timestamp.encode("utf-8"))]
+            })
+
         return JSONResponse(content={"status": "success"}, status_code=200)
     
     except Exception as e:
